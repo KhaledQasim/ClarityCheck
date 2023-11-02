a40608713ddb049af22da578f23f7950ce497151
Khaled Qasim Kraken <khaled.qasim183@gmail.com>
2023-11-02 15:01:26 +0000

    Added back index.php code

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+    include("config/connection.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset= "utf-8" />
+    <title> Clarity Check Login </title>
+    <link href = "css/layout.css" rel = "stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+    <body>
+        
+    <!-- Form and CSS styling copied from https://www.w3schools.com/howto/howto_css_login_form.asp -->
+        <form action="" method="GET"> <!-- Action - go to addcontent.php -->
+        <div class="container">
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
+
+            <label for="password"><b>Password</b></label>
+
+            <!-- Mitigation - Type password hides passwords from being shown in clear text and prevents shoulder surfing -->
+            <input type="password" placeholder="Enter Password" name="password" required>
+            
+            <button name="submit" type="submit">Login</button>
+            <a href="./pages/signup.php"> Don't have an account? </a>
+        </div>
+
+        </form>
+
+        <?php
+
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                if (isset($_GET['submit'])) {
+
+                    $username = $_GET["username"];
+                    $password = $_GET["password"];
+
+                    // Query vulnerbale to SQL Injection
+
+                    //$sql = "SELECT * FROM users WHERE username='". $username . "' AND password='" . $password . "';";
+                    //$result = mysqli_query($con, $sql);
+
+                    // Mitigation - Parameters are prepared and statement is executed
+                    $params = array($username);
+                    $result = $con->execute_query("SELECT * FROM users WHERE username=?", $params);
+
+                    if (mysqli_num_rows($result) > 0 ) {
+                        $user_data = mysqli_fetch_assoc($result);
+                        $hashed_password = $user_data["password"];
+
+                        if (password_verify($password, $hashed_password)) {
+
+                            echo "Hello " . $username . "!" ;
+                            echo "<br>";
+                            session_start();
+                            $_SESSION['login']=true;
+                            $_SESSION['username'] = $username;
+                        
+                        } else {
+                            print("Password is invalid");
+                        }
+
+                    } else {
+                        print("Username or password is invalid");
+                    }
+                }
+            }
+        ?>
+
+    </body>
+</html>
